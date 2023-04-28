2d73bcd9fd69a8e34849e33aed2a5a7484b5a6d5
Kirpal Singh <mailme.kirpal@gmail.com>
2023-04-28 10:43:30 +0530

    useEffect all types of effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [width, setWidth] = useState(window.innerWidth);
 
-  const increamentCount = () => setCount((prevCount)=> ++prevCount)
+  const increamentCount = () => setCount((prevCount) => ++prevCount);
 
-  const decrementCount = () => setCount((prevCount)=> --prevCount)
+  const decrementCount = () => setCount((prevCount) => --prevCount);
+
+  useEffect(() => {
+    console.log("This UseEffect will run each time");
+  });
+
+  useEffect(() => {
+    console.log("This UseEffect will run only first time");
+  }, []);
+
+  useEffect(() => {
+    console.log("This UseEffect will run only if count value gets changed");
+  }, [count]);
+
+  useEffect(() => {
+    const resizeWidth = () => {
+      setWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeWidth);
+    return () => {
+      window.removeEventListener("resize", resizeWidth);
+    };
+  });
 
   return (
     <>
       <div>
-      <h2>{count}</h2>
-      </div>     
+        <h2>{count}</h2>
+        <h2>Width: {width}</h2>
+      </div>
       <div className="card">
-        <button onClick={increamentCount}>
-          Increament
-        </button>
-        <button onClick={decrementCount}>
-          Decreament
-        </button>
-      </div>     
+        <button onClick={decrementCount}>Decreament</button>
+        <button onClick={increamentCount}>Increament</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
